10628fb245fcfa76ecfa071edbf5d5290dfbeb81
Thomas Böhm <thomasboehm111@gmail.com>
2022-09-23 16:07:52 +0200

    Apply dropout after activation

--- net/gnn.py
+++ net/gnn.py
@@ -9,10 +9,8 @@ class GNNModel(nn.Module):
     def __init__(self, embed_dim, output_dim, num_proxies, num_heads, device):
         super().__init__()
 
-        self.att = geom_nn.GATConv(embed_dim, embed_dim, heads=num_heads) 
-        #self.att = MultiHeadDotProduct(embed_dim, nhead=num_heads, aggr='add', dropout=0.1)
-
-        self.act = nn.ReLU()
+        self.attention = geom_nn.GATConv(embed_dim, embed_dim, heads=num_heads) 
+        #self.attention = MultiHeadDotProduct(embed_dim, nhead=num_heads, aggr='add', dropout=0.1)
 
         #self.linear1 = nn.Linear(embed_dim, 4*embed_dim)
         #self.linear2 = nn.Linear(4*embed_dim, embed_dim)
@@ -24,6 +22,7 @@ class GNNModel(nn.Module):
         self.dropout1 = nn.Dropout(0.1)
         #self.dropout2 = nn.Dropout(0.1)
 
+        self.activation = nn.ReLU()
         self.fc = nn.Linear(num_heads * embed_dim, output_dim)
 
         self.proxies = nn.parameter.Parameter(torch.randn((num_proxies, embed_dim))).to(device)
@@ -37,12 +36,11 @@ class GNNModel(nn.Module):
         # connect every sample with every proxy
         edge_index = self.get_edge_index(feats).to(self.device)
 
-        feats = self.att(feats, edge_index)
+        feats = self.attention(feats, edge_index)
+        feats = self.activation(feats)
         feats = self.dropout1(feats)
-        feats = self.act(feats)
-        
-        #feats = self.norm1(feats + feats2)
 
+        #feats = self.norm1(feats + feats2)
         #feats2 = self.linear2(self.dropout_mlp(self.act_mlp(self.linear1(feats))))
         #feats2 = self.dropout2(feats2)
         #feats = self.norm2(feats + feats2)
