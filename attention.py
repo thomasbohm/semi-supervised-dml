4e45123aec0d0f2e12a1ace46cff007f44cf6f87
Thomas Böhm <thomasboehm111@gmail.com>
2022-12-19 15:01:05 +0100

    Load models

--- attention.py
+++ attention.py
@@ -1,5 +1,8 @@
 from PIL import Image
+from net.gnn import GNNModel
 from torchvision import transforms
+from net.load_net import load_resnet50
+import torch
 
 img = Image.open('./data/CARS/images/001/000018.jpg')
 
@@ -18,6 +21,38 @@ transform_train = [
 transform_train = transforms.Compose(transform_train)
 
 img = transform_train(img)
+img = img[None,:,:,:]
 
-print(img.shape)
-print(img[None,:,:,:].shape)
+print('Loaded img with shape', img.shape)
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+resnet, embed_dim = load_resnet50(
+    num_classes=98,
+    pretrained_path='./results/CARS/2022-11-19_02:19:16/CARS_10.0_best.pth',
+    reduction=4,
+    neck=False
+)
+resnet.to(device)
+print('Loaded ResNet.')
+
+
+
+gnn = GNNModel(
+    device,
+    embed_dim = embed_dim,
+    output_dim = 98,
+    num_layers = 1,
+    num_heads = 6,
+    num_proxies = 98,
+    add_mlp = False,
+    gnn_conv = 'GAT',
+    gnn_fc = False,
+    reduction_layer = False
+)
+gnn = gnn.to(device)
+gnn.load_state_dict(torch.load('./results/CARS/2022-11-19_02:19:16/CARS_10.0_gnn_best.pth'))
+print('Loaded GNN.')
+print(gnn)
